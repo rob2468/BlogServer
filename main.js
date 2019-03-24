368aa1a3ca1a3da28a715afe307e51f4b042b54e
稚勉 <zhimian.cj@antfin.com>
2019-03-24 19:29:11 +0800

    使用 mysql 存储评论

--- main.js
+++ main.js
@@ -1,122 +1,177 @@
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
-var url = require('url');
-var domain = require('domain');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const url = require('url');
+const domain = require('domain');
 const exec = require('child_process').exec;
+const mysql = require('mysql');
+const moment = require('moment');
 
-var requestListener = function (request, response) {
-    // 解析请求，包括文件名
-    var pathname = url.parse(request.url).pathname;
+/**
+ * 请求监听
+ * @param {object} request
+ * @param {object} response
+ */
+async function requestListener(request, response) {
+  // 解析请求，包括文件名
+  const parsedURL = url.parse(request.url, true);
+  const pathname = parsedURL.pathname;
+  const query = parsedURL.query;
 
-    // 输出请求的文件名
-    console.log('Request received.\n    Path: ' + pathname + '\n    Method: ' + request.method);
+  // 输出请求的文件名
+  console.log(`Request received.\n    Path: ${pathname}\n    Query: ${JSON.stringify(query)}\n    Method: ${request.method}`);
 
-    // 服务器需要设置的响应头
-    var responseHeaders = {'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type'};
+  // 服务器需要设置的响应头
+  const responseHeaders = {'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type'};
 
-    // 请求合法性检查
-    if (request.method === 'OPTIONS') {
+  if (request.method === 'OPTIONS') {
+    response.writeHead(200, responseHeaders);
+    response.end();
+  } if (pathname === '/index.html') {
+    responseHeaders['Content-Type'] = 'text/html';
+    response.writeHead(200, responseHeaders);
+    const indexContent = fs.readFileSync('index.html', 'utf-8');
+    response.write(indexContent);
+    response.end();
+  } else if (pathname.indexOf('/tmp') === 0) {
+    // 不允许请求 tmp 内文件
+    response.writeHead(404, responseHeaders);
+    response.end();
+  } else if (pathname.indexOf('/api/comments') === 0) {
+    // 查询评论
+    const pageID = query['page_id'];
+    const comments = await queryComments(pageID);
+
+    response.writeHead(200, responseHeaders);
+    response.write(JSON.stringify(comments));
+    response.end();
+  } else if (pathname.indexOf('/api/submitcomment') === 0) {
+    // 客户端提交评论
+    // domain
+    var localDomain = domain.create();
+    localDomain.on('error', function(err) {
+      // 异常捕获
+      response.writeHead(200, responseHeaders);
+      response.write(JSON.stringify({
+        success: false,
+      }));
+      response.end();
+    });
+    localDomain.run(function () {
+      // run
+      var body = '';
+      request.on('data', function (data) {
+        body += data;
+      });
+      request.on('end', function () {
+        if (body.length === 0) {
+          throw new Error('评论读取失败');
+        }
+        // 前端发来的数据
+        const postData = JSON.parse(body);
+        const pageID = postData.pageID;
+        const email = postData.email;
+        const time = postData.time;
+        const displayName = postData.displayName;
+        const content = postData.content;
+        const comment = {
+          pageID,
+          email,
+          time,
+          displayName,
+          content,
+        };
+        addComment(comment);
+
+        // 响应客户端请求
         response.writeHead(200, responseHeaders);
+        response.write(JSON.stringify({
+          success: true,
+          result: comment,
+        }));
         response.end();
-    } else if (pathname.indexOf('/tmp') === 0) {
-        // 不允许请求 tmp 内文件
-        response.writeHead(404, responseHeaders);
-        response.end();
-    } else if (pathname.indexOf('/api/submitcomment') === 0) {
-        // 客户端提交评论
-        // 常量
-        var BRANCH_NAME = 'comments';
-        var OWNER_NAME = 'rob2468';
-        var REPO_NAME = 'rob2468.github.io';
+      }); // request on end
+    }); // domain run
+  }
+}
 
-        // 读取配置文件
-        var conf = fs.readFileSync('conf.json', 'utf-8');
-        var confJSON = JSON.parse(conf);
-        var repoPath = confJSON.path;       // 博客仓库本地路径
+/**
+ * 增加评论
+ */
+function addComment(comment) {
+  // 连接数据库
+  const dbConnection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'Blog'
+  });
+  dbConnection.connect();
 
-        // domain
-        var localDomain = domain.create();
-        localDomain.on('error', function(err) {
-            // 异常捕获
-            response.writeHead(200, responseHeaders);
-            var result = {
-                'error': 1,
-                'message': err.message
-            };
-            response.write(JSON.stringify(result));
-            response.end();
-        });
-        localDomain.run(function () {
-            // run
-            var body = '';
-            request.on('data', function (data) {
-                body += data;
-            });
-            request.on('end', function () {
-                if (body.length === 0) {
-                    throw new Error('评论读取失败');
-                }
-                // 前端发来的数据
-                var postData = JSON.parse(body);
-                var pageID = postData.page_id;
-                var email = postData.email;
-                var date = postData.date;
-                var displayName = postData.display_name;
-                var content = postData.content;
-                // 评论数据模型
-                var commentInfo = [{
-                    'email': email,
-                    'date': date,
-                    'author': {
-                        'display_name': displayName,
-                    },
-                    'content': content
-                }];
-                var comment = {
-                    pageID : commentInfo
-                };
+  // 解析出数据库字段
+  const pageID = comment.pageID;
+  const email = comment.email;
+  let time = comment.time;
+  time = moment(time).format('YYYY-MM-DD HH:mm:ss');
+  const displayName = comment.displayName;
+  const content = comment.content;
 
-                // 创建文件夹
-                const commentFileDirectory = repoPath + '/_data/raw_comments/';
-                if (!fs.existsSync(commentFileDirectory)) {
-                    fs.mkdirSync(commentFileDirectory);
-                }
+  // 构造 sql 语句
+  const sqlStr = `insert into comments ( page_id, email, time, display_name, content ) VALUES ( '${pageID}', '${email}', '${time}', '${displayName}', '${content}' );`;
 
-                // 序列化，并写入文件
-                const commentStr = JSON.stringify(comment);
-                const commentFilePath = commentFileDirectory + 'comment_' + date;
-                fs.writeFileSync(commentFilePath, commentStr);
+  // 执行 sql
+  dbConnection.query(sqlStr, function (error, results, fields) {
+  });
+  dbConnection.end();
+}
 
-                // 调用 shell 脚本，提交评论并推送到 GitHub
-                exec('bash commit.sh ' + repoPath, function (error, stdout, stderr) {
-                    console.log('stdout:');
-                    console.log(stdout);
+/**
+ * 查询评论
+ */
+async function queryComments(pageID) {
+  return new Promise((resolve, reject) => {
+    // 连接数据库
+    const dbConnection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '123456',
+      database : 'Blog'
+    });
+    dbConnection.connect();
 
-                    console.log('stderr:');
-                    console.log(stderr);
-                });
+    // 构造 sql 语句
+    const sqlStr = `select * from comments where page_id = '${pageID}' order by time desc;`;
 
-                // 响应客户端请求
-                response.writeHead(200, responseHeaders);
-                // var link = 'https://github.com/' + OWNER_NAME + '/' + REPO_NAME + "/commit/" + newHEADID;
-                var link = '';
-                var result = {
-                    'error': 0,
-                    'message': '成功',
-                    'link': link
-                };
-                response.write(JSON.stringify(result));
-                response.end();
-            }); // request on end
-        }); // domain run
-    }
-};
+    // 执行 sql
+    dbConnection.query(sqlStr, (error, results, fields) => {
+      const comments = [];
+      results.forEach(element => {
+        const comment = {};
+        comment.pageID = element['page_id'];
+        comment.email = element['email'];
+        const time = element['time'];
+        comment.time = (new Date(time)).getTime();;
+        comment.displayName = element['display_name'];
+        comment.content = element['content'];
+        comments.push(comment);
+      });
+      resolve(comments);
+    });
+    dbConnection.end();
+  });
+}
+
 // 创建服务器
-var server = http.createServer(requestListener);
-var portNum = 8888;
+const server = http.createServer(requestListener);
+const portNum = 8888;
 server.listen(portNum);
 
 // 控制台会输出以下信息
 console.log('Server running at Port ' + portNum);
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', reason.stack || reason)
+  // Recommended: send the information to sentry.io
+  // or whatever crash reporting service you use
+});
